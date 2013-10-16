8ecf06714cf4f404caf4f16c5e9e72ce397e5ea8
unknown <administrator@quanApp.avroTechnik.com>
2013-10-15 19:48:23 -0600

    - chaanged all scripts to be consistent with account name of   backupGTservice

--- backupGTservice_install.bat
+++ backupGTservice_install.bat
@@ -1,8 +1,8 @@
 Copssh_4.0.2_Installer.exe  /S
 cwRsync_4.0.5_Installer.exe /S
 
-net user AvmaxBackupService /add
-net localgroup Administrators AvmaxBackupService /add
-net user AvmaxBackupService /random
+net user backupGTservice /add
+net localgroup Administrators backupGTservice /add
+net user backupGTservice /random
 reg.exe IMPORT "hideusername.reg"
 reg.exe IMPORT "hideaccounts.reg"
